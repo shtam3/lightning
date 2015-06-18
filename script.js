fc85bb7b743aa6c58766e7d4b27fc0bd4c2ec5f3
Shtam <stedman.tam@gmail.com>
2015-06-17 20:35:25 -0400

    site

---
+++ script.js
@@ -0,0 +1,16 @@
+$(function() {
+    $('#nav-button').click(function() {
+        $('header ul').slideToggle();
+    })
+})
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
